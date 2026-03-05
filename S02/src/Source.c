bf69d5e0ae2311f83e7c07e9b409c583eaf9ba0b
IanyAlmasanu <iany4all@gmail.com>
2026-03-05 11:34:49 +0200

    updated S03 activity

--- S02/src/Source.c
+++ S02/src/Source.c
@@ -4,15 +4,32 @@
 #include "Student.h"
 #pragma pack(1) //needed for avoiding padding (2 bytes), packing the memory used to 10 bytes
 
-
 #define LINE_BUFFER 256 //buffer for reading lines from the file with max length of 256
 
-
-
+// Student ** * is passed with an additional * as the array is being modified in real time
+Student** insertIntoArray(int* size, Student** parray, Student stud) {
+	Student** tmp = parray;
+	++(*size);
+	parray = (Student**)malloc((sizeof(Student**)) * (*size));
+	if ((*size - 1) > 0) {
+		for (register int i = 0; i < *size - 1; i++) {
+			parray[i] = tmp[i]; //this copies the initial data into the new location
+		}
+		free(tmp);
+	}
+	parray[(*size) - 1] = &stud;
+	return parray;
+}
 
 int main() {
 
 	FILE* fp = fopen("data.txt", "r");
+	Student array[6]; 
+	//Student* pArray[6]; //static array of 6 pointers
+
+	//Student* array; //ptr  to a Student or an array of elements of type Student
+	Student** parray = NULL; //ptr to a pointer to a Student (?)
+	int size = 0;
 
 	if (fp != NULL) {
 		char line[LINE_BUFFER];
@@ -23,6 +40,7 @@ int main() {
 		short int groupNo;
 		char* token = NULL;
 		char* context = NULL;
+		short index = 0;
 		while (fgets(line, LINE_BUFFER, fp)) {
 			token = strtok_s(line, delimiter, &context); //context is char*, function asks for char**
 			//for this to work we pass the address of the pointer to a char, written as &context = & char* = char**
@@ -31,10 +49,9 @@ int main() {
 			groupNo = atoi(token);
 			token = strtok_s(NULL, delimiter, &context);
 			Student* stud = createStudent(regNo, groupNo, token);
-			//arrayStatic[1] = *stud;
-			//*(arrayStatic + 1) = *stud;
-			//(arrayStatic + 1)[0] = *stud; //re-basing the array
-
+			
+			parray = insertIntoArray(&size, parray, *stud);
+			//parray = instertIntoArray(&size, parray, stud);
 
 			printStudent(stud);
 			deleteStudent(stud);
@@ -48,4 +65,9 @@ int main() {
 
 }
 
+
+
+
+
+//HW: process
 
