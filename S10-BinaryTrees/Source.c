f22c9490aa612159a9f9ac0ce774ed54d44366c2
IanyAlmasanu <iany4all@gmail.com>
2026-04-30 10:58:11 +0300

    Update Source.c

--- S10-BinaryTrees/Source.c
+++ S10-BinaryTrees/Source.c
@@ -3,22 +3,91 @@
 #include <string.h>
 #include <stdlib.h>
 #pragma pack(1)
+#define LINE_BUFFER 256
+
 typedef struct Student {
 	unsigned int regNo;
 	short int groupNo;
 	char* name;
 } Student, * PStudent;
 //typedef struct Student Student;
-#define LINE_BUFFER 256
+
+typedef struct Bst {
+	Student* data;
+	struct BinarySearchTree* leftChild;
+	struct BinarySearchTree* rightChild;
+}BinarySearchTree, *pBinarySearchTree;
 
 Student* createStudent(unsigned int, short int, const char*);
 void printStudent(Student*);
 void deleteStudent(Student*);
 
+BinarySearchTree* createNode(Student* stud) {
+	BinarySearchTree* node = (BinarySearchTree*)malloc(sizeof(BinarySearchTree));
+	if (node == NULL) return;
+
+	node->data = stud;
+	node->leftChild = NULL;
+	node->rightChild = NULL;
+
+	return node;
+}
+
+BinarySearchTree* insertNode(BinarySearchTree* tree, Student* stud) {
+	if (tree == NULL) return createNode(stud);
+	if (tree->data->regNo == NULL) return NULL;
+	else {
+		if (tree->data->regNo > stud->regNo) {
+			tree->rightChild = insertNode(tree->rightChild, stud);
+		}
+		else if (tree->data->regNo < stud->regNo) {
+			tree->leftChild = insertNode(tree->leftChild, stud);
+		}
+		else {
+			Student* tmp = tree->data;
+			tree->data = stud;
+			deleteStudent(stud);
+		}
+
+		return tree;
+	}
+}
+
+void inOrder(BinarySearchTree* root) {
+	if (root != NULL) {
+		inOrder(root->leftChild);
+		printStudent(root->data);
+		inOrder(root->rightChild);
+	}
+}
+
+void deleteKey(BinarySearchTree** tree, int key) {
+	if ((*tree)->data->regNo > key) {
+		deleteKey(&(*tree)->leftChild, key);
+	}
+	
+	else if ((*tree)->data->regNo < key) {
+		deleteKey(&(*tree)->rightChild, key);
+	}
+	
+	else {
+		if ((*tree)->leftChild == NULL && (*tree)->rightChild == NULL) {
+			deleteStudent((*tree)->data);
+			free(*tree);
+			tree = NULL;
+		}
+		else if ((*tree)->leftChild == NULL || (*tree)->rightChild == NULL) {
+
+		}
+	}
+}
+
+
 int main()
 {
-	FILE* fp = fopen("Data.txt", "r");
+	pBinarySearchTree root = NULL;
 
+	FILE* fp = fopen("Data.txt", "r");
 	if (fp != NULL)
 	{
 		char line[LINE_BUFFER];
@@ -40,10 +109,16 @@ int main()
 			if (token != NULL && token[0] == ' ') token++;
 
 			Student* stud = createStudent(regNo, groupNo, token);
-			//token is kept as last field, as declaring a new field for the array is pointless and wastes resources.
-			printStudent(stud);
-			deleteStudent(stud);
+			
+			root = insertNode(root, stud);
+
+			
+
+
 		}
+		inOrder(root);
+		deleteKey(&root, 3500);
+		inOrder(root);
 	}
 }
 void deleteStudent(Student* pStud)
@@ -91,7 +166,7 @@ Student* createStudent(
 			if (pStud->name != NULL) //defensive programming as stated above
 			{
 				strcpy_s(pStud->name, strlen(name) + 1, name);
-				printf("Succesfully created student with name: %s\n", pStud->name);
+				//printf("Succesfully created student with name: %s\n", pStud->name);
 			}
 			else
 			{
